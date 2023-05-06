a71e39d92cf7c633ac4b6cd9d555f258d133f3d2
joonasoispuu <oispuu.joonas@gmail.com>
2023-05-06 12:58:51 +0300

    Added a break timer for users and made the workout page a little bit more pretty

--- Views/absworkoutPage.xaml.cs
+++ Views/absworkoutPage.xaml.cs
@@ -9,6 +9,7 @@ namespace WorkoutApp.Views
         private int _totalExercises;
         private System.Timers.Timer _timer;
 
+
         public absworkoutPage()
         {
             InitializeComponent();
@@ -16,6 +17,13 @@ namespace WorkoutApp.Views
             _dbContext = new ExerciseDbContext();
 
             _dbContext.Database.EnsureCreated();
+
+            GetReadyText.IsVisible = false;
+            NextExerciseText.IsVisible = false;
+            ExerciseBreakTImer.IsVisible = false;
+            NextExerciseName.IsVisible = false;
+            NextExerciseSeconds.IsVisible = false;
+            NextExerciseReps.IsVisible = false;
 
             var exercises = _dbContext.Exercises.ToList();
             foreach (var exercise in exercises)
@@ -33,12 +41,12 @@ namespace WorkoutApp.Views
                     new ExerciseModel { Name = "Jumping Jacks", Reps = 0, Seconds = 30, Category = "Abs" },
                     new ExerciseModel { Name = "Heel Touch ", Reps = 30, Seconds = 0, Category = "Abs" },
                     new ExerciseModel { Name = "V-up", Reps = 0, Seconds = 16, Category = "Abs" },
-                    new ExerciseModel { Name = "Crucnhes", Reps = 30, Seconds = 0, Category = "Abs" },
+                    new ExerciseModel { Name = "Crunches", Reps = 30, Seconds = 0, Category = "Abs" },
                     new ExerciseModel { Name = "Flutter kicks", Reps = 0, Seconds = 40, Category = "Abs" },
                     new ExerciseModel { Name = "Alt V-up", Reps = 0, Seconds = 16, Category = "Abs" },
                     new ExerciseModel { Name = "Push-up&rotation ", Reps = 0, Seconds = 24, Category = "Abs" },
                     new ExerciseModel { Name = "Mountain Climbers", Reps = 30, Seconds = 0, Category = "Abs" },
-                    new ExerciseModel { Name = "V-cruch", Reps = 0, Seconds = 10, Category = "Abs" },
+                    new ExerciseModel { Name = "V-crunch", Reps = 0, Seconds = 10, Category = "Abs" },
                     new ExerciseModel { Name = "Seated abs clockwise circle", Reps = 0, Seconds = 16, Category = "Abs" },
                     new ExerciseModel { Name = "Seated abs counterclockwise circles", Reps = 0, Seconds = 16, Category = "Abs" },
                     new ExerciseModel { Name = "Plank ", Reps = 0, Seconds = 60, Category = "Abs" },
@@ -59,7 +67,7 @@ namespace WorkoutApp.Views
 
         private void UpdateExerciseDisplay()
         {
-            ExerciseNameLabel.Text = $"Exercise: {_currentExercise.Name}";
+            ExerciseNameLabel.Text = $"{_currentExercise.Name}";
 
             if (_currentExercise.Reps != 0)
             {
@@ -111,19 +119,67 @@ namespace WorkoutApp.Views
             _currentExercise.Completed = true;
             _dbContext.SaveChanges();
 
+
             if (_exerciseCount < _totalExercises)
             {
                 // Get the next exercise in database
                 _currentExercise = _dbContext.Exercises.FirstOrDefault(x => x.Category == "Abs" && !x.Completed);
                 _exerciseCount++;
 
+                // Breaktimer that sets all the text except ExerciseBreakTimer as invisible until the timer is complete
+                ExerciseTimerLabel.IsVisible = false;
+                ExerciseNameLabel.IsVisible = false;
+                ExerciseRepsLabel.IsVisible = false;
+                ExerciseCountLabel.IsVisible = false;
+                ExerciseText.IsVisible = false;
+                DoneButton.IsVisible = false;
+
+                int breaktimer = 5;
+
+                while (breaktimer >= 1)
+                {
+                    GetReadyText.IsVisible = true;
+                    NextExerciseText.IsVisible = true;
+                    ExerciseBreakTImer.IsVisible = true;
+                    NextExerciseName.IsVisible = true;
+
+                    if(_currentExercise.Reps != 0)
+                    {
+                        NextExerciseReps.IsVisible = true;
+                        NextExerciseReps.Text = $"Reps: {_currentExercise.Reps}";
+                    }
+
+                    if (_currentExercise.Seconds != 0)
+                    {
+                        NextExerciseSeconds.IsVisible = true;
+                        NextExerciseSeconds.Text = $"Timer: {_currentExercise.Seconds}s";
+                    }
+                    NextExerciseName.Text = _currentExercise.Name;
+                    ExerciseBreakTImer.Text = breaktimer.ToString();
+                    await Task.Delay(1000);
+                    breaktimer--;
+                }
+
+                GetReadyText.IsVisible = false;
+                NextExerciseReps.IsVisible = false;
+                NextExerciseSeconds.IsVisible = false;
+                NextExerciseText.IsVisible = false;
+                ExerciseBreakTImer.IsVisible = false;
+                NextExerciseName.IsVisible = false;
+
+                DoneButton.IsVisible = true;
+                ExerciseTimerLabel.IsVisible = true;
+                ExerciseNameLabel.IsVisible = true;
+                ExerciseRepsLabel.IsVisible = true;
+                ExerciseCountLabel.IsVisible = true;
+                ExerciseText.IsVisible = true;
+
                 UpdateExerciseDisplay();
             }
+
             else
             {
-                await DisplayAlert("Workout Complete", "Congratulations, you have completed the abs workout routine!", "OK");
-
-                // Reset the data
+                await DisplayAlert("Workout Complete", "Congratulations, you have completed the Abs workout routine!", "OK");
                 var exercises = _dbContext.Exercises.ToList();
                 foreach (var exercise in exercises)
                 {
--- Views/customexerciseTrainPage.xaml.cs
+++ Views/customexerciseTrainPage.xaml.cs
@@ -17,6 +17,13 @@ namespace WorkoutApp.Views
 
             _dbContext.Database.EnsureCreated();
 
+            GetReadyText.IsVisible = false;
+            NextExerciseText.IsVisible = false;
+            ExerciseBreakTImer.IsVisible = false;
+            NextExerciseName.IsVisible = false;
+            NextExerciseSeconds.IsVisible = false;
+            NextExerciseReps.IsVisible = false;
+
             var exercises = _dbContext.Exercises.ToList();
             foreach (var exercise in exercises)
             {
@@ -35,7 +42,7 @@ namespace WorkoutApp.Views
 
         private void UpdateExerciseDisplay()
         {
-            ExerciseNameLabel.Text = $"Exercise: {_currentExercise.Name}";
+            ExerciseNameLabel.Text = $"{_currentExercise.Name}";
 
             if (_currentExercise.Reps != 0)
             {
@@ -86,17 +93,65 @@ namespace WorkoutApp.Views
             _currentExercise.Completed = true;
             _dbContext.SaveChanges();
 
+
             if (_exerciseCount < _totalExercises)
             {
                 _currentExercise = _dbContext.Exercises.FirstOrDefault(x => x.Category == "Custom" && !x.Completed);
                 _exerciseCount++;
 
+                ExerciseTimerLabel.IsVisible = false;
+                ExerciseNameLabel.IsVisible = false;
+                ExerciseRepsLabel.IsVisible = false;
+                ExerciseCountLabel.IsVisible = false;
+                ExerciseText.IsVisible = false;
+                DoneButton.IsVisible = false;
+
+                int breaktimer = 5;
+
+                while (breaktimer >= 1)
+                {
+                    GetReadyText.IsVisible = true;
+                    NextExerciseText.IsVisible = true;
+                    ExerciseBreakTImer.IsVisible = true;
+                    NextExerciseName.IsVisible = true;
+
+                    if (_currentExercise.Reps != 0)
+                    {
+                        NextExerciseReps.IsVisible = true;
+                        NextExerciseReps.Text = $"Reps: {_currentExercise.Reps}";
+                    }
+
+                    if (_currentExercise.Seconds != 0)
+                    {
+                        NextExerciseSeconds.IsVisible = true;
+                        NextExerciseSeconds.Text = $"Timer: {_currentExercise.Seconds}s";
+                    }
+                    NextExerciseName.Text = _currentExercise.Name;
+                    ExerciseBreakTImer.Text = breaktimer.ToString();
+                    await Task.Delay(1000);
+                    breaktimer--;
+                }
+
+                GetReadyText.IsVisible = false;
+                NextExerciseReps.IsVisible = false;
+                NextExerciseSeconds.IsVisible = false;
+                NextExerciseText.IsVisible = false;
+                ExerciseBreakTImer.IsVisible = false;
+                NextExerciseName.IsVisible = false;
+
+                DoneButton.IsVisible = true;
+                ExerciseTimerLabel.IsVisible = true;
+                ExerciseNameLabel.IsVisible = true;
+                ExerciseRepsLabel.IsVisible = true;
+                ExerciseCountLabel.IsVisible = true;
+                ExerciseText.IsVisible = true;
+
                 UpdateExerciseDisplay();
             }
+
             else
             {
-                await DisplayAlert("Workout Complete", "Congratulations, you have completed your custom exercises!", "OK");
-
+                await DisplayAlert("Workout Complete", "Congratulations, you have completed your Custom made workout routine!", "OK");
                 var exercises = _dbContext.Exercises.ToList();
                 foreach (var exercise in exercises)
                 {
